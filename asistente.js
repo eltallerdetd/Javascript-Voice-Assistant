e5edd2d262a43f57020fda75faae85f81e21a634
tadeo <tadeodonegana@gmail.com>
2020-02-20 20:16:48 -0300

    Initial Commit

---
+++ asistente.js
@@ -0,0 +1,79 @@
+/*
+Programa creado por youtube.com/ElTallerDeTD
+20/2/20
+*/
+
+
+if (annyang) {
+
+    //Variable para almacenar las voces de nuestro sistema.
+    var voices;
+
+    //Inicializamos utter.
+    var utter = new SpeechSynthesisUtterance();
+    utter.rate = 1;
+    utter.pitch = 0.5;
+    utter.lang = 'es-AR';
+
+    //Cargamos las voces que tenemos en nuestro sistema y las mostarmos en un arreglo por consola.
+    window.speechSynthesis.onvoiceschanged = function () {
+        voices = window.speechSynthesis.getVoices();
+        console.log(voices);
+    };
+
+    //Definimos los comandos a utilizar.
+    var commands = {
+        'hola maria': function () {
+            utter.text = 'Hola usuario';
+            //Setea la voz que queremos usar en base a nuestra lista.
+            utter.voice = voices[2];
+            window.speechSynthesis.speak(utter);
+        },
+        'como estas': function () {
+            utter.text = 'Muy bien!';
+            utter.voice = voices[2];
+            window.speechSynthesis.speak(utter);
+        },
+        'hola': function () {
+            utter.text = 'hola, cual es tu nombre?';
+            utter.voice = voices[2];
+            window.speechSynthesis.speak(utter);
+            //Guarda el nombre que le decimos por voz.
+            annyang.addCallback('result', function (phrases) {
+                //Imprime el nombre por consola.
+                console.log("Nombre: ", phrases[0]);
+                //Para el evento result.
+                annyang.removeCallback('result');
+                //Nos dice hola + el nombre que le digamos por voz.
+                utter.text = 'Hola, ' + phrases[0];
+                window.speechSynthesis.speak(utter);
+            });
+        },
+        //Array que devuelve aleatoriamente un elemento del array, en este caso un chiste.
+        'cuentame un chiste': function () {
+            chistes = ['Por qué las focas del circo miran siempre hacia arriba?   Porque es donde están los focos.',
+                'Estas obsesionado con la comida!   No se a que te refieres croquetamente.',
+                'Por que estás hablando con esas zapatillas?   Porque pone "converse"',
+                'Buenos dias, me gustaria alquilar "Batman Forever".   No es posible, tiene que devolverla tomorrow.'
+            ];
+            utter.text = chistes[Math.floor(Math.random() * chistes.length)]
+            utter.voice = voices[2];
+            window.speechSynthesis.speak(utter);
+        }
+    };
+
+    //Esto nos sirve para ver que escucha el programa en tiempo real.
+    /*
+    annyang.addCallback('result', function(phrases) {
+      console.log("I think the user said: ", phrases[0]);
+      console.log("But then again, it could be any of the following: ", phrases);
+       });
+       */
+
+
+    //Sumamos todos los comandos a annyang.
+    annyang.addCommands(commands);
+
+    //Annyang comienza a escuchar.
+    annyang.start({ autoRestart: false, continuous: true });
+}
